83296c7f44a9eb1fae739fcc0f7470ff56cd1343
Alexandre <alexandre.acustica@gmail.com>
2023-11-06 17:47:35 -0300

    bull

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const emailController = require("../components/emailController");
+
+const errorHandler = require("../utils/errorHandling");
 const router = express.Router();
 
 router.post("/", emailController.validateEmailInput, emailController.Email);
-
+router.use(errorHandler);
 module.exports = router;
