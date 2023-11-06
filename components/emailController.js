671f0a3dc9bc3d09631657fa88d5fca2abcd4945
Alexandre <alexandre.acustica@gmail.com>
2023-11-06 16:54:31 -0300

    db transactions

--- components/emailController.js
+++ components/emailController.js
@@ -1,75 +1,107 @@
 const databaseConn = require("../db");
 const nodemailer = require("nodemailer");
 
-exports.Email = async (req, res, next) => {
-  try {
-    const { nome, email, telefone, content } = req.body;
-    newContent = content.join(" , ");
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    if (!emailRegex.test(email)) {
-      throw new Error("Formato de email inválido");
-    }
+// Função auxiliar para iniciar uma transação no banco de dados
+async function beginTransaction(db) {
+  return new Promise((resolve, reject) => {
+    db.beginTransaction((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+// Função auxiliar para fazer o commit de uma transação no banco de dados
+async function commitTransaction(db) {
+  return new Promise((resolve, reject) => {
+    db.commit((err) => {
+      if (err) {
+        db.rollback(() => {
+          reject(err);
+        });
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+// Função auxiliar para inserção no banco de dados dentro de uma transação
+async function insertDataIntoDatabase(db, nome, email, telefone, content) {
+  return new Promise((resolve, reject) => {
+    const newContent = content.join(" , ");
     const sql =
       "INSERT INTO `decorfab_teste`.`email_teste` (nome, email_cliente, telefone_cliente, content) VALUES (?, ?, ?, ?)";
     const values = [nome, email, telefone, newContent];
 
-    const queryPromise = () => {
-      return new Promise((resolve, reject) => {
-        const db = databaseConn();
-        db.connect((err) => {
-          if (err) {
-            console.error("MySQL connection error:", err);
-            return;
-          }
-          console.log("Connected to MySQL database");
+    db.query(sql, values, (err, result) => {
+      if (err) {
+        db.rollback(() => {
+          reject(err);
         });
-        db.query(sql, values, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-        db.end((err, result) => {
-          console.log("Conexão encerrada com sucesso");
-        });
-      });
-    };
-    try {
-      const result = await queryPromise();
-    } catch (dbError) {
-      console.error("Erro no banco de dados:", dbError);
-      res.status(500).json({ error: "Erro interno do servidor" });
-      return; // Encerre a função aqui para evitar a execução adici
-    }
-
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
+      } else {
+        resolve(result);
+      }
     });
+  });
+}
 
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
-      subject: "Novo Cliente em Contato",
-      text: `Cliente: ${nome}. Email: ${email}; Telefone: ${telefone} deseja contratar os serviços ${newContent} da CodeCase. Entre em contato!`,
-    };
+// Função auxiliar para envio de e-mail
+async function sendEmail(nome, email, telefone, content) {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-    try {
-      // Tente enviar o e-mail
-      await transporter.sendMail(mailOptions);
-      console.log("E-mail enviado com sucesso");
-    } catch (emailError) {
-      console.error("Erro ao enviar e-mail:", emailError);
-      throw new Error("Erro ao enviar e-mail");
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: "Novo Cliente em Contato",
+    text: `Cliente: ${nome}. Email: ${email}; Telefone: ${telefone} deseja contratar os serviços ${content} da CodeCase. Entre em contato!`,
+  };
+
+  try {
+    // Tente enviar o e-mail
+    await transporter.sendMail(mailOptions);
+    console.log("E-mail enviado com sucesso");
+  } catch (emailError) {
+    console.error("Erro ao enviar e-mail:", emailError);
+    throw new Error("Erro ao enviar e-mail");
+  }
+}
+
+exports.Email = async (req, res, next) => {
+  const db = databaseConn();
+
+  try {
+    const { nome, email, telefone, content } = req.body;
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+    if (!emailRegex.test(email)) {
+      throw new Error("Formato de email inválido");
     }
+
+    await beginTransaction(db); // Iniciar a transação no banco de dados
+
+    await insertDataIntoDatabase(db, nome, email, telefone, content); // Inserir dados na tabela dentro da transação
+
+    await sendEmail(nome, email, telefone, content); // Enviar o e-mail
+
+    await commitTransaction(db); // Confirmar a transação no banco de dados
 
     res.status(200).json({ message: "Dados inseridos com sucesso" });
   } catch (error) {
-    console.error("Erro na inserção de dados no MySQL:", error);
-    res.status(500).json({ error: "Erro interno do servidor" });
+    console.error("Erro no controller:", error);
+    db.rollback(() => {
+      res.status(500).json({ error: "Erro interno do servidor" });
+    });
+  } finally {
+    db.end();
   }
 };
